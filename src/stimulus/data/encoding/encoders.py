3cc611aa1637d002b0d8bb0dc6915c663fdfc06d
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2024-11-27 16:57:59 +0100

    minor linting fixes

--- src/stimulus/data/encoding/encoders.py
+++ src/stimulus/data/encoding/encoders.py
@@ -87,7 +87,6 @@ class AbstractEncoder(ABC):
     def _check_data_islist(self, data: list) -> Any:
         """Helper function for checking if the data is a list.
 
-
         Args:
             data (list): a list of data points
 
